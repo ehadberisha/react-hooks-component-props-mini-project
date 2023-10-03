3137839a0bb05c442d121766c52dead316a9a117
Joseph Szpigiel <josephszpigiel@gmail.com>
2023-10-03 12:10:45 -0400

    bonus completed

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,30 @@
 import React from "react";
 
 function Article(props) {
+  const minutes = props.minutes
+  const readLengthArray= []
+  let readLengthLine = ''
+
+
+  if(minutes<30){
+    const closestFives = Math.ceil(minutes / 5);
+    for ( let i = 0; i < closestFives; i++){
+      readLengthArray.push('☕️')
+    }
+    readLengthLine = ` • ${readLengthArray.join('')} ${minutes} minutes to read`
+  }else{
+    const closestTens = Math.ceil(minutes / 10);
+    for ( let i = 0; i < closestTens; i++){
+      readLengthArray.push('🍱')
+    }
+    readLengthLine = ` • ${readLengthArray.join('')} ${minutes} minutes to read`
+  }
+
+
   return (
     <article>
       <h3>{props.title}</h3>
-      <small>{props.date || "January 1, 1970"}</small>
+      <small>{props.date || "January 1, 1970"}{readLengthLine}</small>
       <p>{props.preview}</p>
     </article>
   );
